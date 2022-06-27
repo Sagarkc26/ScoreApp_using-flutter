87d93e39a0e711b01c8cf4d411e9316bf10965cd
Sagar k.c <sagarkc45172@gmail.com>
2022-06-27 15:01:41 +0545

    Score App

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ScoreApp());
+}
+
+class ScoreApp extends StatelessWidget {
+  const ScoreApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "ScoreApp",
+      home: ScoreHome(),
+    );
+  }
+}
+
+class ScoreHome extends StatefulWidget {
+  const ScoreHome({Key? key}) : super(key: key);
+
+  @override
+  State<ScoreHome> createState() => _ScoreHomeState();
+}
+
+class _ScoreHomeState extends State<ScoreHome> {
+  int _counter = 0;
+  void incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ScoreApp"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Score is",
+              style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "$_counter",
+              style: TextStyle(
+                  fontSize: 85,
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      incrementCounter();
+                    },
+                    child: Text("increase")),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      decrementCounter();
+                    },
+                    child: Text("decrease")),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
